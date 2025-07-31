2788d3ba1bfda1d821c5844a205902ff73707fcd
swetha-nethmi <swetha@excellygen.io>
2025-08-01 00:33:27 +0530

    start developing nav bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { assets } from '../assets/assets'
+import { MenuIcon } from 'lucide-react'
+
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
+    <div className='fixed top-0 left-0 z-50 w-full flex items-center justify-between px-6 md:px-16 lg:px-36 py-5'>
+      <Link to='/' className='max-md:flex-1'>
+      <img src={assets.logo} alt="" className='w-36 h-auto' />
+      </Link>
+
+      <div>
+
+      </div>
+
+      <div>
+
+      </div>
+
+      <MenuIcon className='max-md:ml-4 md:hidden w-8 h-8 cursor-pointer'/>
     </div>
   )
 }
