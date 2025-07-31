54795be262bbc344cd0e4e889f8464eae9f99146
swetha-nethmi <swetha@excellygen.io>
2025-08-01 00:03:17 +0530

    create react vite app; folder structure; libraries; packages

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import Home from './pages/Home'
+import Movies from './pages/Movies'
+import MovieDetails from './pages/MovieDetails'
+import SeatLayout from './pages/SeatLayout'
+import MyBookings from './pages/MyBookings'
+import Favourite from './pages/Favourite'
+import { Toaster } from 'react-hot-toast'
+
+const App = () => {
+  const isAdminRoute = useLocation().pathname.startsWith('/admin')
+
+  return (
+    <>
+    <Toaster/>
+        {/* only display if not the admin page */}
+      {!isAdminRoute && <Navbar/>} 
+  
+      <Routes>
+        <Route path='/' element={Home}/>
+        <Route path='/movies' element={Movies}/>
+        <Route path='/movies/:id' element={MovieDetails}/>
+        <Route path='/movies/:id/:date' element={SeatLayout}/>
+        <Route path='/my-bookings' element={MyBookings}/>
+        <Route path='/favourite' element={Favourite}/>
+      </Routes>
+      {!isAdminRoute && <Footer/>} 
+    </>
+  )
+}
+
+export default App
