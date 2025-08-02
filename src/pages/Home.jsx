38382baec763210f6250f593a71bb29a62918592
swetha-nethmi <swetha@excellygen.io>
2025-08-02 18:44:21 +0530

    feat: creat movie cards in homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import HeroSection from "../components/HeroSection";
+import { FeatherIcon } from "lucide-react";
+import FeatureSection from "../components/FeatureSection";
 
 const Home = () => {
   return (
     <>
       <HeroSection />
+      <FeatureSection />
     </>
   );
 };
